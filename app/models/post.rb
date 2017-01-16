6ca4bb913ccd529131ba38cc3e3e7c19723c60ae
Daniel Leonard <deleonard@albany.edu>
2017-01-16 09:35:30 +0000

    now has images yet to resize however

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
     has_many :comments, dependent: :destroy
     has_many :taggings, dependent: :destroy
     has_many :tags, through: :taggings, dependent: :destroy
-    
+    has_many :attachments
     def tag_list
         self.tags.collect do |tag|
             tag.name
@@ -13,5 +13,7 @@ class Post < ActiveRecord::Base
         new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
         self.tags = new_or_found_tags
     end
-
+    
+    has_attached_file :image
+    validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170113074123) do
+ActiveRecord::Schema.define(version: 20170116091512) do
+
+  create_table "attachments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.string   "name"
@@ -26,8 +31,12 @@ ActiveRecord::Schema.define(version: 20170113074123) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "taggings", force: :cascade do |t|
